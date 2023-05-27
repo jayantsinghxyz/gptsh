ffedc74be8a14303e86ca5ffb84698b530c3db00
Jayant Singh <jayantsinghxyz@gmail.com>
2023-05-27 06:06:23 +0530

    Added configuration support

---
+++ config.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type config struct {
+	config map[string]string
+	path string
+}
+
+func NewConfig(path string, filename string) *config {
+	configPath := fmt.Sprintf("%s/%s", path, filename)
+	configData, err := godotenv.Read(configPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {}
+	}
+	return &config{config: configData, path: configPath}
+}
+
+func (ctx config) getOpenaiApiKey() (key string, err error) {
+	apiKey := ctx.config["OPENAI_API_KEY"]
+	if len(apiKey) == 0 {
+		return "", os.ErrNotExist
+	}
+	return apiKey, nil
+}
+
+func (ctx config) askOpenaiApiKey() (key string, err error) {
+	fmt.Print("Openai API Key: ")
+	var apiKey string
+	fmt.Scan(&apiKey)
+	err = ctx.setOpenaiApiKey(apiKey)
+	if err != nil {
+		return "", err
+	}
+	return ctx.getOpenaiApiKey()
+}
+
+func (ctx config) setOpenaiApiKey(key string) error {
+	ctx.config["OPENAI_API_KEY"] = key
+	err := ctx.writeConfig()
+	return err
+}
+
+func (ctx config) writeConfig() error {
+	configString, err := godotenv.Marshal(ctx.config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile(ctx.path, []byte(configString), 0644)
+	return err
+}
--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var flagApiKey string
+	
+	app := &cli.App{
+	Name:  "gptsh",
+	Usage: "Use GPT from shell",
+	EnableBashCompletion: true,
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:        "setApiKey",
+			Usage:       "Set Openai API Key",
+			Destination: &flagApiKey,
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		if (len(flagApiKey) != 0) {
+			fmt.Println("Work in progress")
+			return nil
+			} else {
+				homeDir, err := os.UserHomeDir()
+				if (err != nil) {
+					fmt.Println(err)
+				}
+				
+				configData := NewConfig(homeDir, ".gptsh")
+				apiKey, err := configData.getOpenaiApiKey()
+				if err != nil {
+					if (errors.Is(err, os.ErrNotExist)) {
+						apiKey, err = configData.askOpenaiApiKey()
+						if err != nil {
+							log.Fatal(err)
+						}
+					} else {
+						log.Fatal(err)
+					}
+				}
+				
+				fmt.Println(flagApiKey, apiKey)
+				fmt.Println("ctx.App.Name", ctx.Args().Slice(), flagApiKey)
+				return nil	
+			}
+		},
+	}
+	
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
